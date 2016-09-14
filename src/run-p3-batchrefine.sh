865330426460f7aea93ba3ee07a647cd83479898
Ondrej Hlavacek <ondrej.hlavacek@keboola.com>
2016-09-14 09:41:49 +0200

    fix: use remote openrefine backend

--- src/run-p3-batchrefine.sh
+++ src/run-p3-batchrefine.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
-# supervisord -c /code/supervisor/supervisord.conf
-# sleep 10
+supervisord -c /code/supervisor/supervisord.conf
+supervisorctl start openrefine
+sleep 30
 jq -r ".parameters.script[0]" /data/config.json  > /data/transformation.json
-/app/p3-batchrefine/bin/batchrefine embedded /data/in/tables/data.csv /data/transformation.json /data/out/tables/data.csv
+/app/p3-batchrefine/bin/batchrefine remote /data/in/tables/data.csv /data/transformation.json /data/out/tables/data.csv
 
